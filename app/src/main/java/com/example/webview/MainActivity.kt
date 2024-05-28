e8808f2da7daecbed25d8782591eafdf1e82cb3c
syed-growexxer <syed.raza@growexx.com>
2024-05-28 19:35:30 +0530

    feat: 🎸 First Commit

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.webview
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webViewSetup(webView)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun webViewSetup(webView: WebView) {
+        webView.webViewClient = WebViewClient()
+        webView.apply {
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+            loadUrl("https://docs.flutter.dev/release/archive?tab=macos")
+        }
+    }
+}
